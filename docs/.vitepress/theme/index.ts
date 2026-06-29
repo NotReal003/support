c86c504e565d66a1f806b4e3885a74334fe11ace
NotReal <117504040+NotReal003@users.noreply.github.com>
2026-06-29 13:09:20 +0530

    Add custom theme for VitePress documentation

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,14 @@
+import { h } from 'vue'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import './custom.css'
+
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {})
+  },
+  enhanceApp({ app, router, siteData }) {
+    //
+  }
+} satisfies Theme
